f032bad7982b5ddc230cfd219298ab57314aa267
Sameer kumar <2006Sameer2009@gmail.com>
2024-05-25 09:33:13 +0530

    formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,18 @@
 module.exports = {
     extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended-type-checked',
-        'prettier'
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended-type-checked",
+        "prettier",
     ],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
     parserOptions: {
         project: true,
         tsconfigRootDir: __dirname,
     },
     root: true,
-    rules:{
+    rules: {
         "no-console": "error",
         "dot-notation": "error",
-    }
-  };
+    },
+};
