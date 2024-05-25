251b3de598e67167a5cd1f2fb557eb224b2b8237
Sameer kumar <2006Sameer2009@gmail.com>
2024-05-25 07:02:17 +0530

    setup prettier

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ const app = express();
 
 const PORT = 8000;
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}..`);
+    console.log(`server is running on port ${PORT}..`);
 });
